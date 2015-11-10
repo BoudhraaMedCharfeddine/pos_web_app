eae10fba7d7d9b98cd4ed0a23dffc7d3daf2d8e1
Charf <boudhraa.med.charfeddine@gmail.com>
2015-11-10 23:29:43 +0100

    change connexion

--- config/connexion.php
+++ config/connexion.php
@@ -1,14 +1,10 @@
 <?php
 class DbConnexion {
-/*	private $servername = "46.101.165.157";
+	private $servername = "46.101.165.157";
 	private $username = "root";
 	private $password = "charf1990";
 	private $dbname = "test_exec";
-*/	
-	private $servername = "localhost";
-	private $username = "root";
-	private $password = "";
-	private $dbname = "trattoufi";
+
 	
 	public function connect() {
 		$conn = new mysqli ( $this->servername, $this->username, $this->password, $this->dbname );
